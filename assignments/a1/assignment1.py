8e1078e085c84fda8b9b2ffae7d5f7e2b18633ca
Luis Fernandes <sednanref2004@gmail.com>
2018-04-22 21:51:29 +0200

    Exercise 1_4 done.

--- assignments/a1/assignment1.py
+++ assignments/a1/assignment1.py
@@ -111,7 +111,7 @@ def exercise1_3(arms):
 	plt.title('Average Reward after 2000 chosen actions.')
 	plt.xticks(range(0, 2000, 100))	
 	#plt.show()
-	plt.savefig('1_2_1_Avg_Reward')
+	plt.savefig('1_3_1_Avg_Reward')
 	plt.close()
 
 	legend = []
@@ -129,7 +129,118 @@ def exercise1_3(arms):
 	plt.legend(legend, loc='lower right')
 	plt.xticks(range(0, 2000, 100))	
 	#plt.show()
-	plt.savefig('1_2_2_Usage_Percentage')
+	plt.savefig('1_3_2_Usage_Percentage')
+	plt.close()
+
+def exercise1_4(arms):
+
+	# Expected Values
+	q = [[0], [0], [0], [0], [0], [0], [0]]
+	# Times selected
+	ks = [0, 0, 0, 0, 0, 0, 0]
+	ks_hundred = [[0,0], [0,0], [0,0], [0,0], [0,0], [0,0], [0,0]]
+	hundred = 1
+	# Epsilon
+	eps = 0.1
+
+	avg = [0]
+
+	# take random 
+	best = random.randrange(0, len(arms))
+
+	for i in range(0,2000):
+
+		if (i == 1000):
+			arms[3] = [6,8]
+
+		# Get arm selection percentage
+		if (i % 100 == 0):
+			if i != 0:
+				for j in range(0, len(ks)):
+					ks_hundred[j].append(0)
+
+				hundred = hundred + 1
+					
+
+		selection = random.random()
+		#print selection
+		# Exploration case
+		if selection < eps:
+			# arm selected
+			k = random.randrange(0, len(arms))
+			# reward
+			reward = random.uniform(arms[k][0], arms[k][1])
+			# take last q_k
+			last = q[k][-1]
+			# Obtain new  expected value for arm k
+			qk = last + (1.0 / (ks[k] + 1)) * (reward - last)
+			# Append new reward for selected arm
+			q[k].append(qk)
+			# update number of times arm k was selected
+			ks[k] = ks[k] + 1
+			ks_hundred[k][hundred] = ks_hundred[k][hundred] + 1
+			
+			# Get general avg
+			new_avg = avg[-1] + (1.0 / (i + 1) * (reward - avg[-1]))
+			avg.append(new_avg)
+			# Update best arm:
+			best = getBest(q)
+			
+		else:
+			# reward
+			reward = random.uniform(arms[best][0], arms[best][1])
+			# take last q_k
+			last = q[best][-1]
+			# Obtain new  expected value for arm k
+			qk = last + (1.0 / (ks[best] + 1)) * (reward - last)
+			# Append new reward for selected arm
+			q[best].append(qk)
+			# update number of times arm k was selected
+			ks[best] = ks[best] + 1
+			ks_hundred[best][hundred] = ks_hundred[best][hundred] + 1
+			
+			# Get general avg
+			new_avg = avg[-1] + (1.0 / (i + 1) * (reward - avg[-1]))
+			avg.append(new_avg)
+			# Update best arm:
+			best = getBest(q)
+
+
+	# legend = [] 
+	# for i in range(len(q)):
+	# 	plt.plot(q[i])
+	# 	legend.append('Arm ' + str(i + 1))
+
+
+	# plt.legend(legend, loc='lower right')
+
+
+	plt.plot(avg)
+	plt.title('Average Reward after 2000 chosen actions.')
+	plt.xticks(range(0, 2000, 100))	
+	plt.show()
+	plt.savefig('1_4_1_Avg_Reward')
+	plt.close()
+
+	legend = []
+	### Plot usage percentages
+	xaxis = range(0,2100, 100)
+	for i in range(len(ks_hundred)):
+	 	plt.plot(xaxis, ks_hundred[i])
+	 	legend.append('Arm ' + str(i + 1))
+
+
+	plt.title('Usage percentage every 100 steps')
+	plt.xticks()
+	plt.ylabel('Usage percentage')
+	plt.xlabel('Number of Actions taken')
+	plt.legend(legend, loc='lower right')
+	plt.xticks(range(0, 2000, 100))	
+	plt.show()
+	plt.savefig('1_4_2_Usage_Percentage')
+	plt.close()
+
+
 
 
 def main():
@@ -139,7 +250,11 @@ def main():
 	exercise1_2(arms)
 
 	exercise1_3(arms)
-	# print exercise1_1(arms)
+
+	exercise1_4(arms)
+
+	exercise1_5(arms)
+	
 
 if __name__ == "__main__":
     main()
